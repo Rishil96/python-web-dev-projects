95e625bec467c8e82dcd85699c952f6c5eb4d94b
Rishil Ramesh <rishil.ramesh@outlook.com>
2024-11-04 16:17:22 +0530

    Add : Script to practice advanced selenium features

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ email_field.send_keys(email)
 time.sleep(1)
 password_field.send_keys(password)
 time.sleep(1)
-
+print("All fields filled successfully")
 
 # 2. Upload file
 file_path = str(pathlib.WindowsPath(os.getcwd(), "data.txt"))
@@ -33,6 +33,7 @@ print(file_path)
 file_upload = driver.find_element(by=By.ID, value="fileUpload")
 file_upload.send_keys(file_path)
 time.sleep(1)
+print("File uploaded successfully")
 
 # 3. Select an option
 drop_down = driver.find_element(by=By.ID, value="options")
@@ -44,6 +45,7 @@ select.select_by_value("option1")
 time.sleep(1)
 select.select_by_index(2)
 time.sleep(1)
+print("Dropdowns switched successfully")
 
 # 4. Click Buttons
 buttons = driver.find_elements(by=By.TAG_NAME, value="button")
@@ -57,22 +59,27 @@ buttons[1].click()
 time.sleep(1)
 
 driver.find_element(by=By.ID, value="popup").click()
+print("Buttons clicked successfully")
 
 # 5. Click link
 link = driver.find_element(by=By.ID, value="dynamicContent")
 link.click()
 time.sleep(1)
+print("Link clicked successfully")
 
 # 6. Interact with IFrame
-iframe = driver.find_element(by=By.ID, value="div#iframeContainer iframe")
+iframe = driver.find_element(by=By.CSS_SELECTOR, value="div#iframeContainer iframe")
 driver.switch_to.frame(iframe)
+print("Switched context to IFrame successfully")
 
 iframe_link = driver.find_element(by=By.TAG_NAME, value="a")
 iframe_link.click()
 time.sleep(1)
+print("IFrame content accessed successfully")
 
 driver.switch_to.default_content()
 driver.execute_script("alert('Selenium OP!')")
+print("JS executed successfully")
+time.sleep(5)
 
 print("Interaction complete!")
-
