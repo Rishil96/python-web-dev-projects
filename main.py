48d5883f2ff09b237c412ebfc74d1dd474480f71
Rishil Ramesh <rishil.ramesh@outlook.com>
2024-11-12 15:16:34 +0530

    Add : Custom blog title and subtitles for each blogs

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
+import requests
 from flask import Flask, render_template
+
+# Load blogs
+BLOG_URL = "https://api.npoint.io/99e68aa5bc1aec0bd3eb"
+blog_response = requests.get(url=BLOG_URL)
+all_blogs = blog_response.json()
+
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", blogs=all_blogs)
 
 
 @app.route("/about")
