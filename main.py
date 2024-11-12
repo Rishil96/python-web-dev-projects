95a8ff2d1236e7c06c6bcb1956eae11cb6798c76
Rishil Ramesh <rishil.ramesh@outlook.com>
2024-11-12 15:33:03 +0530

    Add : Render individual posts on clicking

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import requests
 from flask import Flask, render_template
 
 # Load blogs
-BLOG_URL = "https://api.npoint.io/99e68aa5bc1aec0bd3eb"
+BLOG_URL = "https://api.npoint.io/b0ebb78475df0f4c9d31"
 blog_response = requests.get(url=BLOG_URL)
 all_blogs = blog_response.json()
+print(all_blogs)
 
 
 app = Flask(__name__)
@@ -23,6 +24,16 @@ def about():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+
+@app.route("/post/<int:pid>")
+def post(pid):
+    current_post = {}
+    for blog in all_blogs:
+        if blog["id"] == pid:
+            current_post = blog
+            break
+    return render_template("post.html", post=current_post)
 
 
 if __name__ == "__main__":
