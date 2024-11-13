1b9caf66213ecc75ece69ec8bfee031af79689be
Rishil Ramesh <rishil.ramesh@outlook.com>
2024-11-13 14:51:22 +0530

    Add : Error logging in login template and add validations for each field

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, redirect
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, Length
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=8)])
     submit = SubmitField('Login')
 
 
@@ -19,9 +19,10 @@ def home():
     return render_template('index.html')
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
     login_form = LoginForm()
+    # Check validations added in our forms when form is submitted
     if login_form.validate_on_submit():
         return redirect('/home')
     return render_template('login.html', form=login_form)
