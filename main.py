29362f01f0f9ee66a2aace69a6d4b02ca51ef161
Rishil Ramesh <rishil.ramesh@outlook.com>
2024-11-14 16:13:15 +0530

    Add : Bootstrap Flask to the project

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ class CafeForm(FlaskForm):
 # Exercise:
 # add: Location URL, open time, closing time, coffee rating, wifi rating, power outlet rating fields
 # make coffee/wifi/power a select element with choice of 0 to 5.
-#e.g. You could use emojis ☕️/💪/✘/🔌
+# e.g. You could use emojis ☕️/💪/✘/🔌
 # make all fields required except submit
 # use a validator to check that the URL field has a URL entered.
 # ---------------------------------------------------------------------------
