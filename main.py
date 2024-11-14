58d35198ce239045a27832f78871feb4cc8cc943
Rishil Ramesh <rishil.ramesh@outlook.com>
2024-11-14 16:28:54 +0530

    Add : Changes to display list of cafes on /cafes endpoint

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ class CafeForm(FlaskForm):
     submit = SubmitField('Submit')
 
 # Exercise:
-# add: Location URL, open time, closing time, coffee rating, wifi rating, power outlet rating fields
+# add: Location URL, open time, closing time, coffee rating, Wi-Fi rating, power outlet rating fields
 # make coffee/wifi/power a select element with choice of 0 to 5.
 # e.g. You could use emojis ☕️/💪/✘/🔌
 # make all fields required except submit
@@ -60,7 +60,7 @@ def cafes():
         list_of_rows = []
         for row in csv_data:
             list_of_rows.append(row)
-    return render_template('cafes.html', cafes=list_of_rows)
+    return render_template('cafes.html', cafes=list_of_rows[1:])
 
 
 if __name__ == '__main__':
